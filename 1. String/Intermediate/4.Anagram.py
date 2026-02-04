524a63d64d493b8bc6b1e744571a5a5687ef0404
Dipanshi Tewari <dipanshitewari.25@gmail.com>
2026-02-04 20:43:15 +0530

    string problem 4

--- 1. String/Intermediate/4.Anagram.py
+++ 1. String/Intermediate/4.Anagram.py
@@ -1,1 +1,11 @@
 # Check if two strings are anagrams
+s1 = input("Enter first string: ")
+s2 = input("Enter Second string: ")
+check = True
+for i in s1:
+    if i not in s2:
+        print('Two strings are nor Anagrams.')
+        check=False
+        break
+if check:
+    print('Two strings are Anagrams.')
