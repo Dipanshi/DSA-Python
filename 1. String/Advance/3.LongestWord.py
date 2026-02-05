4cc482efd6df43a9f0c159c480aff849124b40fc
Dipanshi Tewari <dipanshitewari.25@gmail.com>
2026-02-05 14:05:23 +0530

    String all questions completed

--- 1. String/Advance/3.LongestWord.py
+++ 1. String/Advance/3.LongestWord.py
@@ -1,1 +1,9 @@
 # Write a program to find the longest word in a sentence
+sentence = input("Enter the sentence to check the longest word: ")
+words = sentence.split(" ")
+word=''
+for w in words:
+    if len(word)<len(w):
+        word=w
+
+print(f"The word with longest lenght is : {word}")
--- 1. String/Advance/4.CaesarCipher.py
+++ 1. String/Advance/4.CaesarCipher.py
@@ -1,1 +1,13 @@
-# Implement a simple Caesar cipher encryption
+# Implement a simple Caesar cipher encryption 
+# It is a substitution cipher where each letter in the plaintext is shifted by a fixed number of positions in the alphabet.
+
+word = input("Enter the word to encrypt: ")
+result=''
+shift=3
+for ch in word:
+    start= ord('A') if ch.isupper() else ord('a')
+    result+= chr((ord(ch)-start + shift)%26 + start)
+
+print(f'The word after encryption is {result}')
+
+
