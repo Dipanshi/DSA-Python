6cd0333d2a2e9e0f10d0ea442b31f1c6a50c6cf1
Dipanshi Tewari <dipanshitewari.25@gmail.com>
2026-02-25 22:04:04 +0530

    Restructuring in folders

--- Data Structures/6. Queue/2. Problems/5. Number of Recent Calls.py
+++ Data Structures/6. Queue/2. Problems/5. Number of Recent Calls.py
@@ -18,8 +18,12 @@ class RecentCounter:
         return len(self.requests)
 
 # Usage:
-rc = RecentCounter()
-print(rc.ping(1))     # 1
-print(rc.ping(100))   # 2
-print(rc.ping(3001))  # 3
-print(rc.ping(3002))  # 3
+# rc = RecentCounter()
+# print(rc.ping(1))     # 1
+# print(rc.ping(100))   # 2
+# print(rc.ping(3001))  # 3
+# print(rc.ping(3002))  # 3
+
+dq=deque([1,2,3,5,7,8,9])
+dq.rotate(-1)
+print(dq)
